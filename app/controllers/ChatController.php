e19a5f3f9bae137eaa303c1f7d8bdce854677623
m28serj <serj.m28@gmail.com>
2015-03-06 12:05:02 +0200

    CodeStyle: minimal changes

--- app/controllers/ChatController.php
+++ app/controllers/ChatController.php
@@ -1,7 +1,9 @@
 <?php
 
 class ChatController extends BaseController {
+
 		public function getIndex() {
+
 				return View::make('index');
 		}
 
@@ -10,20 +12,25 @@ class ChatController extends BaseController {
 				$rules = array(
 						'message'     => 'required|min:2'
 				);
+
 				$validation = Validator::make(['message' => e(Input::get('message'))], $rules);
 				if (!$validation->passes()){
+
 						return Response::json(['status' => 'error',
 																	 'data' => $validation->errors()->toArray(),
 																	 'message' => 'You have some validation errors']);
 				} else {
-						$post = ChatPost::create(['user_id' => Auth::user()->id, 'message' => e(Input::get('message'))]);
-						$post->save();
 
-						return Response::json(['id' =>  $post->id, 200]);
+						$post = ChatPost::create(['user_id' => Auth::user()->id, 'message' => e(Input::get('message'))]);
+
+						return Response::json(['status' => 'success',
+																	 'data' => ['id' =>  $post->id],
+																	 'message' => null]);
 				}
 		}
 
 		public function postPosts() {
+
 				$posts = ChatPost::orderBy('created_at','desc')->limit(10)->get();
 				return View::make('partials/posts')->with('posts', $posts);
 		}
