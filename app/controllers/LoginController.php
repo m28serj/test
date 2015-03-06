a94bfc652dd977cf81eec9a5b51af248cef85749
m28serj <serj.m28@gmail.com>
2015-03-06 12:10:43 +0200

    Reducing SQL queries: eager load users with chat posts

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,25 +3,30 @@
 class LoginController extends BaseController {
 
 		public function login() {
-				$username = trim(e(Input::get('username')));
+
+				$username = e(trim(Input::get('username')));
 				$password = e(Input::get('password'));
 
 				$rules = array(
 						'username'     => 'required|min:3',
 						'password'     => 'required|min:6'
 				);
+
 				$validation = Validator::make(['username' => $username, 'password' => $password], $rules);
 				if (!$validation->passes()){
+
 						return Redirect::back()->withInput()->withErrors($validation->errors());
 				} else {
+
 						if (!Auth::attempt(['username' => $username, 'password' => $password])) {
 
 								if (!count(User::where('username', '=', $username)->get())) {
-										User::create([
-																		 'username' => $username,
-																		 'password' => Hash::make($password)
+
+										User::create(['username' => $username,
+																	'password' => Hash::make($password)
 																 ]);
 										if(Auth::attempt(['username' => $username, 'password' => $password])){
+
 												return Redirect::to('')->with('success', 'You have successfully registered and logged in');
 										} else {
 												return Redirect::back()->with('error', 'Registration error');
@@ -40,6 +45,7 @@ class LoginController extends BaseController {
 		}
 
 		public function logout() {
+
 				Auth::logout();
 
 				return Redirect::to('');
